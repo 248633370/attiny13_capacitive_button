308796e81e9d8d964973dfa57808dd3dd9b46506
Нечаев Антон <antofka@nechet.linuxd.org>
2016-07-10 15:27:59 +0300

    Initial ADC

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 // Nechaev Anton 20.10.2015
-// Capacitive button on ATtiny13/ATtiny85 for led lamp
+// Capacitive button on ATtiny13/ATtiny85 for led lamp, power from atx power supply
 //
 // DDRxn=0 PORTxn=0 – Режим: HI-Z – высоко импендансный вход.
 // DDRxn=0 PORTxn=1 – Режим: PullUp – вход с подтяжкой до лог.1.
@@ -10,10 +10,17 @@
 
 #include <avr/io.h>
 #include <stdbool.h>
+#include <avr/interrupt.h>
 
 // vars for set power on, full and half light
 bool atx_status = false;
 bool full_light = false;
+
+// write digital "high" to pin <pn> on port <prt>
+//#define DIGIWRITE_H(prt, pn) prt |= (1<<pn)
+//
+// write digital "low" to pin <pn> on port <prt>
+//#define DIGIWRITE_L(prt, pn) prt &= ~(1<<pn)
 
 void pwm_setup(void)
 {
@@ -26,12 +33,13 @@ void pwm_setup(void)
 	TCCR0A |= (1<<COM0B1);
 	TCCR0B |= (1<<CS00);
 	// COM0A1 - 1 set "Clear OC0A on Compare Match, set OC0A at TOP"
-	// WGM02:0 - 111 set Fast PWM Mode with TOP in OCRA
-	// CS02:0 - 001 set clock source divider /32
-	OCR0B = 0x80; // Duty about 50% (128 from 255)
+	// WGM02:0 - 0b011 set Fast PWM Mode with TOP in OCRA
+	// CS02:0 - 0b001 set clock source divider /32
+
+	//OCR0B = 0x80; // Duty about 50% (128 from 255)
 }
 
-/*
+
 void adc_setup(void)
 {
 	// config ADC
@@ -39,7 +47,7 @@ void adc_setup(void)
 	ADMUX |= (1<<MUX1);
 	// REFS0 - 0 set Vcc as analog reference
 	// ADLAR - 1 left align, use ADCH register
-	// MUX1:0 - 10 ADC2 (PB4)
+	// MUX1:0 - 0b10 ADC2 (PB4)
 
 	ADCSRA |= (1<<ADPS2)|(1<<ADPS0);
 	ADCSRA |= (1<<ADEN)|(1<<ADSC)|(1<<ADATE)|(1<<ADIE);
@@ -49,7 +57,7 @@ void adc_setup(void)
 	// ADIE - 1 enable ADC interrupt
 	// ADIF - 1 enable
 }
-*/
+
 void pson_switch()
 {
 	if (atx_status)
@@ -69,11 +77,12 @@ void pson_switch()
 
 int main(void)
 {
-	pwm_setup();
-	//adc_setup();
+	//pwm_setup();
+	adc_setup();
+	sei();	// enable global interrupts
 
-	//int adc_current;
-	//int adc_prev;
+	int adc_current;
+	int adc_prev;
 
 	//main loop
 	while (1)
